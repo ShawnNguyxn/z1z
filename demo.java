ec211d720d77f6290a47b3c25ffbb4d95652d268
ShawnNguyxn <sonkonmac@Sonkonmacs-MacBook-Air.local>
2023-05-17 00:35:10 +0900

    init123

---
+++ demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("N = ");
+        int n = scanner.nextInt();
+        int a[] = new int[n]
+
+        for (int i =0, i<n, i++){
+            a[i]= scanner.nextInt();
+        }
+    }
+}
